9542768ebb75275c76665941fe592564c1362393
younguk-lee <younguk.lee@pnesolution.com>
2021-02-06 18:39:59 +0900

    first commit

---
+++ RefMVVMLight/ViewModel/MainViewModel.cs
@@ -0,0 +1,14 @@
+using GalaSoft.MvvmLight;
+
+namespace RefMVVMLight.ViewModel
+{
+
+    public class MainViewModel : ViewModelBase
+    {
+
+        public MainViewModel()
+        {
+
+        }
+    }
+}
